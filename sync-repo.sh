17a11d350a1df2ab7223fec17ac622841b6951bd
varfmx21 <luisvalderrabano9@gmail.com>
2026-03-06 19:12:59 -0600

    feat: last version, delivery complete

--- sync-repo.sh
+++ sync-repo.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
 
+cd /home/c4rnage/projects/forms-app-web || exit 1 # Esta linea debe ser cambiada por la ruta del proyecto
+
 echo "user: $(git -C ./ config user.name)"
 echo "branch: $(git -C ./ rev-parse --abbrev-ref HEAD)"
 echo "[$(date '+%Y-%m-%d %H:%M:%S')]"
 echo "-----"
-echo "Pulling latest changes from Git..."
+echo "[+] Fetching latest changes from Git..."
 git fetch
+echo "[+] Pulling latest changes from Git..."
 git pull
-echo "Code synchronization complete!"
+echo "[+] Code synchronization complete!"
 echo ""
