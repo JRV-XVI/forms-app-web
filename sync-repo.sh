703496f6ad7065b16b9d71ffb2d16175e79b2b42
varfmx21 <luisvalderrabano9@gmail.com>
2026-03-06 18:43:15 -0600

    feat: new version for logs in the sync-repo file

--- sync-repo.sh
+++ sync-repo.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+echo "user: $(git -C ./ config user.name)"
+echo "branch: $(git -C ./ rev-parse --abbrev-ref HEAD)"
+echo "[$(date '+%Y-%m-%d %H:%M:%S')]"
+echo "-----"
 echo "Pulling latest changes from Git..."
 git fetch
 git pull
 echo "Code synchronization complete!"
+echo "\n"
