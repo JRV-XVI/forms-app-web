c6da65c60ec5b31af3e7b170190edc8e502edc41
varfmx21 <luisvalderrabano9@gmail.com>
2026-03-07 20:53:58 -0600

    feat: connect services with controllers - endpoints

--- backend/src/main/java/com/springboot/backend/controller/ControllerComics.java
+++ backend/src/main/java/com/springboot/backend/controller/ControllerComics.java
@@ -1,11 +1,23 @@
 package com.springboot.backend.controller;
 
 import org.springframework.web.bind.annotation.*;
-import com.springboot.backend.services.*;
-import java.util.Map;
+import com.springboot.backend.services.serviceCreateComics;
+import com.springboot.backend.services.serviceGetComics;
 
 @RestController
 @RequestMapping("/api/comics")
 public class ControllerComics {
-    
+
+    private serviceCreateComics serviceCreateComics;
+    private serviceGetComics serviceGetComics;
+
+    @PostMapping
+    public String createComics() {
+        return serviceCreateComics.createComics();
+    }
+
+    @GetMapping
+    public String getComics() {
+        return serviceGetComics.getComics();
+    }
 }
