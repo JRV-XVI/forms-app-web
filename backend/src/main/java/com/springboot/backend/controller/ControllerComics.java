06ac498ab16e9e56c8540af36b43584ee7c49eec
varfmx21 <luisvalderrabano9@gmail.com>
2026-03-07 20:40:26 -0600

    feat: template for controllers for /api/comics

---
+++ backend/src/main/java/com/springboot/backend/controller/ControllerComics.java
@@ -0,0 +1,11 @@
+package com.springboot.backend.controller;
+
+import org.springframework.web.bind.annotation.*;
+import com.springboot.backend.services.*;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/comics")
+public class ControllerComics {
+    
+}
